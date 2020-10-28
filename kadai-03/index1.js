f089b99eb85101dd4745f3f9450a406e56ed1fcf
Torakky <rit62233@gmail.com>
2020-10-28 05:29:52 +0000

    課題3を追加

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,9 +1,9 @@
 // 生まれた日を入力
 const day = parseInt(process.argv[2])
-if (day % 3 === 0) {
+if (day % 3 === 0){
     console.log('あなたはzatsuな性格です')
-} else if (day % 3 === 1) { 
+} else if (day % 3 === 1){ 
     console.log('あなたはtekitouな性格です') 
-] else {
+} else {
     console.log('あなたはsquareな性格です')
 }
