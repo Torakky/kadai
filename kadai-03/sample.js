3bc0da8a73972b7b51eea9a316d17e7893202bcf
Torakky <rit62233@gmail.com>
2020-10-28 05:20:27 +0000

    課題3を追加

---
+++ kadai-03/sample.js
@@ -0,0 +1,24 @@
+const score = 90
+
+if (score >= 90) {
+    console.log('S')
+} else if(score >= 80) {
+    console.log('A')
+} else if(score >= 70) {
+    console.log('B')
+} else if(score >= 60) {
+    console.log('C')
+} else {
+    console.log('F')
+}
+    
+let sum = 0
+for (let index = 1; n <= 100; n = n + 1) {
+    sum = sum + n
+}
+
+
+
+console.log(sum)
+
+    
---
+++ kadai-03/sample2.js
@@ -0,0 +1,6 @@
+let sum = 0
+
+for (let nunber =0; number < 10; number = nunber + 1) {
+    if (number % 3 === 1) {
+        console.log(number)
+    }
