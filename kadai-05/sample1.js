151afbdb63afde8a86f18f55670d7d78545e0800
Torakky <rit62233@gmail.com>
2020-11-18 05:02:36 +0000

    課題5を追加

---
+++ kadai-05/sample1.js
@@ -0,0 +1,14 @@
+const person = {
+    name:'Yoshida',
+    age:20,
+    hasCat: true,
+}
+// prefecture: 'Kyoto',
+// country: 'Japan'
+
+
+
+
+console.log(person.nickname)
+console.log(person.address)
+console.log(person.address.prefecture)
---
+++ kadai-05/sample5.js
@@ -0,0 +1,4 @@
+const obj1 = {a: 10, b: 20 }
+console.log(obj1.a)
+const obj2 = obj1
+// const obj2 = 
