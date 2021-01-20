ce00bda6b1bdfe8f8b43ef745a644395318677fc
Torakky <rit62233@gmail.com>
2021-01-20 03:58:29 +0000

    課題5を追加

--- kadai-05/index.js
+++ kadai-05/index.js
@@ -10,6 +10,7 @@ const keys = Object.keys(profile)
 console.log(keys)
 
 for (let index = 0; index < keys.length; index = index + 1) {
-    const key = keys[index];
-    console.log('私の ' + key + '　は　'　+ profile[key] + '　です。　')
+    const key = keys[index]
+    const value = profile[key]
+    console.log('私の ' + key + ' は '　+ value + ' です。')
 }
